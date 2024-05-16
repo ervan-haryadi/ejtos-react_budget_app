b539a56f198d14aa091b5c62e75f5cdb363802d5
Ervan Haryadi <ervan.a.haryadi@gmail.com>
2024-05-16 13:18:25 +0700

    add budget limit & currency

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,22 +2,34 @@ import { useContext, useState } from "react"
 import { AppContext } from "../context/AppContext"
 
 const Budget = () => {
-    const { dispatch, budget } = useContext(AppContext);
+    const { dispatch, budget, expenses, currency } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total = total + item.cost);
+    }, 0)
     const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
-        // using dispatch to change budget state
-        // BUG: setState ALWAYS uses previously rendered value
-        dispatch({
-            type:"SET_BUDGET",
-            payload:(parseInt(event.target.value))
-        })
+        let eventBudget = event.target.value;
+        if (eventBudget > 20000) {
+            alert("The budget cannot exceed £ 20000!")
+        } else {
+            if (eventBudget < totalExpenses) {
+                alert("You cannot reduce the budget value lower than the spending!")
+            } else {
+                setNewBudget(eventBudget);
+                // using dispatch to change budget state
+                // BUG: setState ALWAYS uses previously rendered value
+                dispatch({
+                    type: "SET_BUDGET",
+                    payload: (parseInt(event.target.value))
+                })
+            }
+        }
     }
 
     return (
         <div className="alert alert-secondary">
-            <span>Budget: $</span>
-            <input type="number" step="10" value={newBudget} onChange={handleBudgetChange} />
+            <span>Budget: {currency} </span>
+            <input type="number" step="10" style={{width:'60%'}} value={newBudget} onChange={handleBudgetChange} />
         </div>
     )
 }
