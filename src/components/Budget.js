46c0ac281f3eb4e1c04eed76e14c73e41d03491d
Ervan Haryadi <ervan.a.haryadi@gmail.com>
2024-05-16 10:39:05 +0700

    fix budget event not updating

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,17 +1,25 @@
-import React, { useContext, useState } from 'react';
-import { AppContext } from '../context/AppContext';
+import { useContext, useState } from "react"
+import { AppContext } from "../context/AppContext"
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { dispatch, budget } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
     const handleBudgetChange = (event) => {
         setNewBudget(event.target.value);
+        // using dispatch to change budget state
+        // BUG: setState ALWAYS uses previously rendered value
+        dispatch({
+            type:"SET_BUDGET",
+            payload:(parseInt(event.target.value))
+        })
     }
+
     return (
-<div className='alert alert-secondary'>
-<span>Budget: £{budget}</span>
-<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
-</div>
-    );
-};
+        <div className="alert alert-secondary">
+            <span>Budget: $</span>
+            <input type="number" step="10" value={newBudget} onChange={handleBudgetChange} />
+        </div>
+    )
+}
+
 export default Budget;
