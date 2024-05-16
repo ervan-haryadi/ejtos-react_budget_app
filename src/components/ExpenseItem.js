15ef6ce1644be1506125f7e17b61c7b41c09a365
Ervan Haryadi <ervan.a.haryadi@gmail.com>
2024-05-16 13:31:59 +0700

    add decrease button

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -24,12 +24,27 @@ const ExpenseItem = (props) => {
         })
     }
 
+    const handleDecreaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: 10
+        }
+
+        dispatch({
+            type: 'RED_EXPENSE',
+            payload: expense
+        })
+    }
+
     return (
         <tr>
             <td>{props.name}</td>
             <td>{props.currency} {props.cost}</td>
             <td>
-                <button onClick={event=> handleIncreaseAllocation(props.name)}>+</button>
+                <button className="btn btn-outline-success fs-5" onClick={event=> handleIncreaseAllocation(props.name)}>+</button>
+            </td>
+            <td>
+                <button className="btn btn-outline-danger fs-5" onClick={event=> handleDecreaseAllocation(props.name)}>-</button>
             </td>
             <td><TiDelete size="1.5em" onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
